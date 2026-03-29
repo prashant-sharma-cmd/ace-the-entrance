e7f7541ece95e375e71b81288723920401a47b5d
Admin <prashant.shar51@gmail.com>
2026-03-29 16:20:09 +0800

    misc: updated the redirect link to daraz

--- ace-the-entrance/home/views.py
+++ ace-the-entrance/home/views.py
@@ -137,7 +137,7 @@ def redirect_to_instagram(request):
     return redirect('https://www.instagram.com/ace_the_entrance/')
 
 def redirect_to_daraz(request):
-    return redirect('https://www.daraz.pk/')
+    return redirect('https://www.daraz.com.np/products/ace-the-entrance-2-entrance-guide-2082-edition-blue-book-by-alumni-of-st-xaviers-college-i312612823.html')
 
 def redirect_to_whatsapp(request):
     return redirect('https://wa.me/85270396856')
