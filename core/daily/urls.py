7b7bae4e15947d8c288e4b7da6a0ba25cac41d87
Prashant Sharma <prashant.shar51@gmail.com>
2026-04-09 15:56:57 +0800

    Revert "docs: update README with automated Docker workflow; refactor: changed the app directory name"

--- core/daily/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from daily import views
-
-app_name = 'daily'
-urlpatterns = [
-    path('quiz/', views.DailyQuizView.as_view(), name='quiz' ),
-    path('quiz/api/questions/', views.DailyQuizAPI.as_view(), name='get_questions' ),
-]
