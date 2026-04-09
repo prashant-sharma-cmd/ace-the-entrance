e4b43fc8314cc0c5a8c862c8d2ce00f5e907ddec
Admin <prashant.shar51@gmail.com>
2026-04-09 12:33:24 +0800

    fix: solved the bg-image and open graph images not rendering properly

--- ace-the-entrance/home/context_processors.py
+++ ace-the-entrance/home/context_processors.py
@@ -2,5 +2,6 @@ from django.conf import settings
 
 def project_settings(request):
     return {
-        'PROJECT_NAME': getattr(settings, 'PROJECT_NAME', 'Default')
+        'PROJECT_NAME': getattr(settings, 'PROJECT_NAME', 'Default'),
+        'REQUEST_PATH': request.path
     }
